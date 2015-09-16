6534430ff844c10be399583d11550ad2b34513f2
Nick Olds <nolds9@yahoo.com>
2015-09-16 00:07:58 -0400

    Working on 'Make an offer' business logic

--- scripts/controllers/browse.js
+++ scripts/controllers/browse.js
@@ -1,10 +1,9 @@
 'use strict';
 
-app.controller('BrowseController', function($scope, $routeParams, toaster, Contract, Auth, Comment) {
+app.controller('BrowseController', function($scope, $routeParams, toaster, Contract, Auth, Comment, Offer) {
 
 	$scope.searchContract = '';
 	$scope.contracts = Contract.all;
-
 	$scope.user = Auth.user;
 
 	$scope.signedIn = Auth.signedIn;
@@ -25,14 +24,76 @@ app.controller('BrowseController', function($scope, $routeParams, toaster, Contr
 		// We check isContractCreator only if user signedIn
 		// so we don't have to check every time normal guests open the contract
 		if($scope.signedIn()) {
+
+			// Check if the current logged in user has already made an offer for selected contract
+			// Check if the current login user has already made an offer for selected contract
+				Offer.isOffered(contract.$id).then(function(data) {
+					console.log(data);
+					$scope.alreadyOffered = data;
+					console.log($scope.alreadyOffered);
+				});
+
 			// Check if the current login user is the creator of selected contract
 			$scope.isContractCreator = Contract.isCreator;
 
 			// Check if the selectedContract is open
 			$scope.isOpen = Contract.isOpen;
+
+			// Unblock the Offer Button
+			$scope.block = false;
+
+			// Check if the current login user is offer maker (to display Cancel Offer button)
+			$scope.isOfferMaker = Offer.isMaker;
 		}
+
+		// Get list of Comments for selected Contract
 		$scope.comments = Comment.comments(contract.$id);
+
+		// Get list of Offers for selected Contract
+		$scope.offers = Offer.offers(contract.$id);
+
 	}
+
+	// --------------- OFFER ---------------
+	$scope.makeOffer = function() {
+		var offer = {
+			total: $scope.data.total,
+			uid: $scope.user.uid,
+			name: $scope.user.profile.name,
+			gravatar: $scope.user.profile.gravatar
+		};
+
+		Offer.makeOffer($scope.selectedContract.$id, offer).then(function() {
+			toaster.pop('success', "Your offer has been placed.");
+
+			// Mark that the current user has offerred for this contract.
+			console.log($scope.alreadyOffered);
+
+			$scope.alreadyOffered = true;
+
+			console.log($scope.alreadyOffered);
+
+
+			// Reset offer form
+			$scope.total = '';
+
+			// Disable the "Offer Now" button on the modal
+			$scope.block = true;
+		});
+	};
+
+	// $scope.cancelOffer = function(offerId) {
+	// 	Offer.cancelOffer($scope.selectedContract.$id, offerId).then(function() {
+	// 		toaster.pop('success', "Your offer has been cancelled.");
+	//
+	// 		// Mark that the current user has cancelled offer for this contract.
+	// 		$scope.alreadyOffered = false;
+	//
+	// 		// Unblock the Offer button on Offer modal
+	// 		$scope.block = false;
+	// 	});
+	// };
+
 
 	// --------------- COMMENT ---------------
 	$scope.addComment = function(){
--- scripts/services/offer.js
+++ scripts/services/offer.js
@@ -1,21 +1,61 @@
 'use strict';
 
-app.factory('Offer', function(FURL, $firebase){
-  var ref = new Firebase(FURL);
+app.factory('Offer', function(FURL, $firebase, $q, Auth) {
+	var ref = new Firebase(FURL);
+	var user = Auth.user;
 
-  var Offer = {
-    offers: function(contractId){
-        return $firebase(ref.child('offers').child(contractId)).$asArray();
-    },
+	var Offer = {
+		offers: function(contractId) {
+			return $firebase(ref.child('offers').child(contractId)).$asArray();
+		},
 
-    makeOffer: function(contractId, offer){
-      var contract_offers = this.offers(contractId);
+		makeOffer: function(contractId, offer) {
+			var contract_offers = this.offers(contractId);
 
-      if(contract_offers){
-        return contract_offers.$add(offer);
-      }
-    }
-  };
+			if(contract_offers) {
+				return contract_offers.$add(offer);
+			}
+		},
 
-  return Offer;
+		//-----------------------------------------------//
+
+		// This function is to check if the login user already made offer for this contract.
+		// This to prevent a user from offering more than 1.
+		isOffered: function(contractId) {
+
+			if(user && user.provider) {
+				var d = $q.defer();
+
+				$firebase(ref.child('offers').child(contractId).orderByChild("uid")
+					.equalTo(user.uid))
+					.$asArray()
+					.$loaded().then(function(data) {
+						d.resolve(data.length > 0);
+					}, function() {
+						d.reject(false);
+					});
+
+				return d.promise;
+			}
+
+		},
+
+		//-----------------------------------------------//
+
+		isMaker: function(offer) {
+			return (user && user.provider && user.uid === offer.uid);
+		},
+
+		getOffer: function(contractId, offerId) {
+			return $firebase(ref.child('offers').child(contractId).child(offerId));
+		},
+
+		cancelOffer: function(contractId, offerId) {
+			return this.getOffer(contractId, offerId).$remove();
+		}
+
+	};
+
+	return Offer;
+
 });
